e9c8e1134d8e6240e7a5cd9cf9462bcfbdbe1ffa
quentin DELBLOND <delblondquentin@gmail.com>
2023-01-10 14:32:05 +0100

    creation commande repo

--- eshopData/src/main/java/eshop/repository/CommandeRepository.java
+++ eshopData/src/main/java/eshop/repository/CommandeRepository.java
@@ -1,5 +1,30 @@
 package eshop.repository;
 
-public class CommandeRepository {
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import eshop.entity.Client;
+import eshop.entity.Commande;
+
+public interface CommandeRepository extends JpaRepository<Commande, Long> {
+
+	List<Commande> findByDate(LocalDate date);
+	
+	List<Commande> findByClientContaining (Client client);
+	
+	@Query("select f from Commande f left join fetch f.achats where f.id=:id")
+	Optional<Commande> findByIdFetchAchats(@Param("id") Long id);
+	
+    @Query("delete Commande c where c.client=:client")
+    @Modifying
+    @Transactional
+    void deleteByCommandes(@Param("client") Commande client);
 }
--- eshopData/src/main/java/eshop/service/CommandeService.java
+++ eshopData/src/main/java/eshop/service/CommandeService.java
@@ -1,5 +1,64 @@
 package eshop.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import eshop.entity.Commande;
+import eshop.exception.CommandeException;
+import eshop.exception.IdException;
+import eshop.repository.CommandeRepository;
+
+
+@Service
 public class CommandeService {
+
+	@Autowired
+	private CommandeRepository commandeRepository;
+	
+	public Commande create(Commande commande) {
+		checkCommandeIsNotNull(commande);
+		if (commande.getAchats().isEmpty() || commande.getAchats() == null) {
+			throw new CommandeException("commande sans achats");
+		}
+		return commandeRepository.save(commande);
+	}
+	
+	private void checkCommandeIsNotNull(Commande commande) {
+		if (commande == null) {
+			throw new CommandeException("commande null");
+		}
+	}
+	
+	public Commande getById(Long id) {
+		if (id == null) {
+			throw new IdException();
+		}
+		return commandeRepository.findById(id).orElseThrow(() -> {
+			throw new CommandeException("commande inconnu");
+		});
+	}
+	
+	public Page<Commande> getAll(Pageable pageable) {
+		if (pageable == null) {
+			throw new CommandeException();
+		}
+		return commandeRepository.findAll(pageable);
+	}
+
+	public Page<Commande> getNextPage(Page<Commande> page) {
+		if (page == null) {
+			throw new CommandeException();
+		}
+		return commandeRepository.findAll(page.nextOrLastPageable());
+	}
+
+	public Page<Commande> getPrevious(Page<Commande> page) {
+		if (page == null) {
+			throw new CommandeException();
+		}
+		return commandeRepository.findAll(page.previousOrFirstPageable());
+	}
 
 }
